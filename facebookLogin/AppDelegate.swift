239a7df5cc1f88bc0f1f987d7b2a780f12321e33
Anton Chugunov <anton.chugunov@migogroup.ru>
2017-04-18 14:23:57 +0300

    facebook login completed

--- facebookLogin/AppDelegate.swift
+++ facebookLogin/AppDelegate.swift
@@ -2,11 +2,12 @@
 //  AppDelegate.swift
 //  facebookLogin
 //
-//  Created by Dmitriy Egorov on 17/04/17.
+//  Created by Anton Chugunov on 17/04/17.
 //  Copyright © 2017 antonson10. All rights reserved.
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,9 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
     }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -34,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- facebookLogin/ViewController.swift
+++ facebookLogin/ViewController.swift
@@ -2,22 +2,90 @@
 //  ViewController.swift
 //  facebookLogin
 //
-//  Created by Dmitriy Egorov on 17/04/17.
+//  Created by Anton Chugunov on 17/04/17.
 //  Copyright © 2017 antonson10. All rights reserved.
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FBSDKLoginButtonDelegate {
+    @IBOutlet weak var facebookUsername: UILabel!
+    @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var facebookLoginButton: FBSDKLoginButton!
+    var facebookAvatar: FBSDKProfilePictureView!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        facebookLoginButton.readPermissions = ["public_profile"]
+        facebookLoginButton.delegate = self
+        drawShadow()
+        if((FBSDKAccessToken.current()) == nil)
+        {
+            facebookUsername.text = "Not authorized!"
+        }
+        else
+        {
+            if(FBSDKAccessToken.current().permissions.contains("public_profile"))
+            {
+                fetchUserProfileData()
+            }
+        }
+    }
+    
+    func drawShadow()
+    {
+        shadowView.layer.cornerRadius = shadowView.frame.size.height * 0.5
+        shadowView.layer.shadowOffset = CGSize.init(width: 5, height: 5)
+        shadowView.layer.shadowRadius = 5
+        shadowView.layer.shadowOpacity = 0.5
+        shadowView.clipsToBounds = false
+        
+        facebookAvatar = FBSDKProfilePictureView.init(frame: shadowView.frame)
+        facebookAvatar.layer.borderWidth = 2.0
+        facebookAvatar.layer.borderColor = UIColor.blue.cgColor
+        facebookAvatar.clipsToBounds = true
+        shadowView.addSubview(facebookAvatar)
+        facebookAvatar.frame.origin = CGPoint.init(x: 0, y: 0)
+        facebookAvatar.layer.cornerRadius = facebookAvatar.frame.size.height * 0.5
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!)
+    {
+        if(error == nil)
+        {
+            if(result.token != nil)
+            {
+                if(result.grantedPermissions.contains("public_profile"))
+                {
+                    fetchUserProfileData()
+                }
+            }
+        }
+    }
+    
+    func fetchUserProfileData()
+    {
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields": "id, name, picture"]).start(completionHandler: { (connection, result, error) in
+            if (error == nil){
+                print(result ?? "hmmmm")
+                let data = result as! NSDictionary
+                self.facebookUsername.text = data.object(forKey: "name") as? String
+                self.facebookAvatar.profileID = data.object(forKey: "id") as? String
+            }
+        })
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!)
+    {
+        facebookUsername.text = "Not authorized!"
     }
 
 
